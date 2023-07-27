193b48bf2a999063e6c938a30ce9867745e71b56
nefilmjp <136662366+nefilmjp@users.noreply.github.com>
2023-07-28 00:25:10 +0900

    Fix next settings for static

--- next.config.js
+++ next.config.js
@@ -5,22 +5,6 @@ const prefix = "/nms-interceptor-catalog";
 const nextConfig = {
   output: "export",
   assetPrefix: prefix,
-  async rewrites() {
-    return [
-      {
-        source: `${prefix}/api/:path*`,
-        destination: "/api/:path*",
-      },
-      {
-        source: `${prefix}/images/:query*`,
-        destination: '/_next/image/:query*'
-      },
-      {
-        source: `${prefix}/_next/:path*`,
-        destination: "/_next/:path*",
-      },
-    ]
-  }
 }
 
 module.exports = nextConfig
