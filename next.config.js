eebf96561b760afaa6106fa9cf540aa8e98cdb32
nefilmjp <136662366+nefilmjp@users.noreply.github.com>
2023-07-28 00:22:30 +0900

    Update next confit for subdirectory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const prefix = "/nms-interceptor-catalog";
+
 const nextConfig = {
-  output: "export"
+  output: "export",
+  assetPrefix: prefix,
+  async rewrites() {
+    return [
+      {
+        source: `${prefix}/api/:path*`,
+        destination: "/api/:path*",
+      },
+      {
+        source: `${prefix}/images/:query*`,
+        destination: '/_next/image/:query*'
+      },
+      {
+        source: `${prefix}/_next/:path*`,
+        destination: "/_next/:path*",
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
